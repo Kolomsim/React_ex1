aca7723dbf1d42b65fa93468690e147f4bab874c
Maxim Kolomiets <gmkolomiets@gmail.com>
2024-09-06 09:20:21 +0400

    Ex 1

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react'
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from 'react-router-dom'
+import RegisterPage from './pages/LoginSignup/Signup.jsx'
+import LoginPage from './pages/LoginSignup/Login.jsx'
+
+function App() {
+	const [authenticated, setAuthenticated] = useState(false)
+	const [loading, setLoading] = useState(true)
+
+	useEffect(() => {
+		const checkAuth = () => {
+			const accessToken = localStorage.getItem('accessToken')
+			if (accessToken) {
+				setAuthenticated(true)
+			}
+			setLoading(false)
+		}
+		checkAuth()
+	}, [])
+
+	if (loading) {
+		return <div>Loading...</div>
+	}
+
+	return (
+		<Router>
+			<Routes>
+				<Route path='/signup' element={<RegisterPage />} />
+				<Route
+					path='/login'
+					element={<LoginPage setAuthenticated={setAuthenticated} />}
+				/>
+				<Route
+					path='*'
+					element={<ProtectedRoutes authenticated={authenticated} />}
+				/>
+			</Routes>
+		</Router>
+	)
+}
+
+function ProtectedRoutes({ authenticated }) {
+	return (
+		<Layout>
+			<Routes>
+				<Route
+					path='/example_link'
+					element={authenticated ? <ExamplePage /> : <Navigate to='/login' />}
+				/>
+			</Routes>
+		</Layout>
+	)
+}
+
+export default App
---
+++ src/pages/LoginSignup/Login.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate, Link } from 'react-router-dom'
+
+export default function LoginPage({ setAuthenticated }) {
+	const [formData, setFormData] = useState({
+		username: '',
+		password: '',
+	})
+	const navigate = useNavigate()
+
+	const handleChange = e => {
+		setFormData({ ...formData, [e.target.name]: e.target.value })
+	}
+
+	const handleSubmit = async e => {
+		e.preventDefault()
+		try {
+			const response = await axios.post(
+				`${import.meta.env.VITE_API_URL}/auth/login`,
+				formData
+			)
+			localStorage.setItem('accessToken', response.data.accessToken)
+			const userData = {
+				id: response.data._id,
+				username: response.data.username,
+			}
+			localStorage.setItem('userData', JSON.stringify(userData))
+			setAuthenticated(true)
+			navigate('/example_page')
+		} catch (error) {
+			console.error('Ошибка при отправке запроса:', error)
+		}
+	}
+
+	return (
+		<div className='LoginBlock'>
+			<div className='LoginInfoBlock'>
+				<h1>Добро пожаловать в WorkNetwork!</h1>
+				<p>
+					Пожалуйста, войдите в свой аккаунт, чтобы продолжить общение с
+					друзьями и коллегами.
+				</p>
+			</div>
+			<div className='loginOrRegister'>
+				<form className='LoginForm' onSubmit={handleSubmit}>
+					<input
+						type='text'
+						name='username'
+						value={formData.username}
+						onChange={handleChange}
+						placeholder='Логин'
+					/>
+					<input
+						type='password'
+						name='password'
+						value={formData.password}
+						onChange={handleChange}
+						placeholder='Пароль'
+					/>
+					<button type='submit'>Отправить</button>
+				</form>
+				<div className='goToRegister'>
+					<Link to='/signup'>
+						<button>Зарегистрироваться</button>
+					</Link>
+					<p>
+						После регистрации вы получите доступ ко всем возможностям
+						WorkNetwork!
+					</p>
+				</div>
+			</div>
+		</div>
+	)
+}
